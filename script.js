9703240ec38cac6a520b697c8f9c88229157a1ed
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-10 14:56:51 +0300

    implementing button toggle to affect DOM

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const onMouseHover = document.getElementsByClassName("notice")[0];
-onMouseHover.addEventListener("mouseover", function () {
-  alert("!!! Site still under construction !!!");
+// const onMouseHover = document.getElementsByClassName("notice")[0];
+// onMouseHover.addEventListener("mouseover", function () {
+//   alert("!!! Site still under construction !!!");
+// });
+
+const onClick = document.getElementsByClassName("notice")[0];
+const extraContent = document.getElementsByClassName("extra")[0];
+onClick.addEventListener("click", function () {
+  if (extraContent.style.display === "none") {
+    extraContent.style.display = "block";
+    addImage("./under-construction.jpg", "⚠️Site Under Construction⚠️");
+  } else {
+    extraContent.style.display = "none";
+  }
 });
+
+function addImage(img_url, h2) {
+  let image = document.querySelector("img");
+  let banner = document.querySelector("h2");
+  image.src = img_url;
+  banner.textContent = h2;
+}
